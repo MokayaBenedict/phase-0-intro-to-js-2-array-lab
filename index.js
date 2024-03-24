90afc52aedc028d0afd82f76c8e46317cbe2f09f
Benedict <mokayaakivatsi@gmail.com>
2024-03-24 22:33:20 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+const cats = ["Milo", "Otis","Garfield",];// Write your solution here!
+function destructivelyAppendCat(name){
+cats.push(name)}
+function destructivelyAppendCat(name){
+cats.unshift(name);}
+function destructivelyRemoveLastCat(){
+cats.shift()}
+function appendCat(name){
+ return[...cats,name]}
+function prependCat(name){
+return ["Arnold", ...cats]}
+function removeLastCat(name){
+return cats.slice(0,2);}
+function removeFirstCat(name){
+return cats.slice(1,3);}
+
